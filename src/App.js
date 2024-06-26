69271f9019a687b29bd590a9c64176c993c90a22
Silvio Júnior <silvio-sjunior@hotmail.com>
2024-06-26 16:11:39 -0400

    fixing routes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/my-portfolio">
       <GlobalStyle />
       <Navbar />
       <Routes>
